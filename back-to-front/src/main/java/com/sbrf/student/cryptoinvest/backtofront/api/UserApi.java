75dd40f7ea94cfeea85050cd5f5399ddfed45d80
erumyantsev <reugen@inbox.ru>
2024-09-17 13:38:15 +0300

    crypto-22: Docker

--- back-to-front/src/main/java/com/sbrf/student/cryptoinvest/backtofront/api/UserApi.java
+++ back-to-front/src/main/java/com/sbrf/student/cryptoinvest/backtofront/api/UserApi.java
@@ -5,6 +5,7 @@ import com.sbrf.student.cryptoinvest.backtofront.models.User;
 import com.sbrf.student.cryptoinvest.backtofront.utils.RestTemplateClass;
 import com.sbrf.student.cryptoinvest.backtofront.utils.UserAuthentication;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -17,9 +18,12 @@ import java.util.Optional;
 @Component
 public class UserApi {
 
-    private final String USERS_API_PATH = "http://localhost:8081/users";
-    private final String ACCOUNT_API_PATH = "http://localhost:8081/account";
+    private final String USERS_API_PATH = "/users";
+    private final String ACCOUNT_API_PATH = "/account";
     private final RestTemplateClass restTemplate;
+
+    @Value("${USERS_URL}")
+    private String BASE_USERS_URL;
 
     @Autowired
     public UserApi(RestTemplateClass restTemplate) {
@@ -32,7 +36,7 @@ public class UserApi {
      * @return опциональный объект клиента
      */
     public Optional<User> getUserByUsername(String username) {
-        ResponseEntity<User> response = restTemplate.getForEntity(USERS_API_PATH + "/" + username, User.class);
+        ResponseEntity<User> response = restTemplate.getForEntity(BASE_USERS_URL + USERS_API_PATH + "/" + username, User.class);
         if (response.getStatusCode() == HttpStatus.OK) {
             return Optional.of(response.getBody());
         } else {
@@ -46,7 +50,7 @@ public class UserApi {
      * @return опциональный объект клиента
      */
     public Optional<User> postUser(User user) {
-        ResponseEntity<User> response = restTemplate.postForEntity(USERS_API_PATH, user, User.class);
+        ResponseEntity<User> response = restTemplate.postForEntity(BASE_USERS_URL + USERS_API_PATH, user, User.class);
         if (response.getStatusCode() == HttpStatus.OK) {
             return Optional.of(response.getBody());
         } else {
@@ -62,7 +66,7 @@ public class UserApi {
         System.out.println("userId: " + currentUserId);
         AccountPutBalanceTopUpDTO putBalanceTopUpDTO = new AccountPutBalanceTopUpDTO(currentUserId, amount);
         restTemplate.put(
-            ACCOUNT_API_PATH + "/topup", putBalanceTopUpDTO, AccountPutBalanceTopUpDTO.class
+                BASE_USERS_URL + ACCOUNT_API_PATH + "/topup", putBalanceTopUpDTO, AccountPutBalanceTopUpDTO.class
         );
     }
 }
