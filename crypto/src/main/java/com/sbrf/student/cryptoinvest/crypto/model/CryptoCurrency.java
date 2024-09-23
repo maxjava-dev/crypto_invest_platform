69b00e6054f4fa5d81aad1c6a1d7c1b0333c09a0
erumyantsev <reugen@inbox.ru>
2024-09-23 13:32:16 +0300

    crypto-32: Дополнительные данные о криптовалюте (изменение цены, marketCap)

--- crypto/src/main/java/com/sbrf/student/cryptoinvest/crypto/model/CryptoCurrency.java
+++ crypto/src/main/java/com/sbrf/student/cryptoinvest/crypto/model/CryptoCurrency.java
@@ -3,6 +3,7 @@ package com.sbrf.student.cryptoinvest.crypto.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.math.BigDecimal;
 
@@ -10,36 +11,60 @@ import java.math.BigDecimal;
  * Модель криптовалюты с метаданными и ценой
  */
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+@RequiredArgsConstructor
 public class CryptoCurrency {
     /**
      * Идентификатор.
      */
-    private Long id;
+    private final Long id;
 
     /**
      * Символьный код криптовалюты.
      */
-    private String symbol;
+    private final String symbol;
 
     /**
      * Название.
      */
-    private String name;
+    private final String name;
 
     /**
      * Описание.
      */
-    private String description;
+    private final String description;
 
     /**
      * Url на картинку.
      */
-    private String logo;
+    private final String logo;
 
     /**
      * Цена в USD.
      */
     private BigDecimal price;
+
+    /**
+     * Рыночная капитализация
+     */
+    private BigDecimal marketCap;
+
+    /**
+     * Изменение цены за последний час в процентах
+     */
+    private BigDecimal percentChange1h;
+
+    /**
+     * Изменение цены за последний день в процентах
+     */
+    private BigDecimal percentChange24h;
+
+    /**
+     * Изменение цены за последние 7 дней в процентах
+     */
+    private BigDecimal percentChange7d;
+
+    /**
+     * Изменение цены за последние 30 дней в процентах
+     */
+    private BigDecimal percentChange30d;
 }
--- crypto/src/main/java/com/sbrf/student/cryptoinvest/crypto/model/data/CoinMarketCapPriceQuote.java
+++ crypto/src/main/java/com/sbrf/student/cryptoinvest/crypto/model/data/CoinMarketCapPriceQuote.java
@@ -1,5 +1,6 @@
 package com.sbrf.student.cryptoinvest.crypto.model.data;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +8,7 @@ import lombok.NoArgsConstructor;
 import java.math.BigDecimal;
 
 /**
- * Цена криптовалюты из CoinMarketCap
+ * Данные о цене криптовалюты из CoinMarketCap
  */
 @Data
 @NoArgsConstructor
@@ -16,5 +17,36 @@ public class CoinMarketCapPriceQuote {
     /**
      * Цена
      */
+    @JsonProperty("price")
     BigDecimal price;
+
+    /**
+     * Рыночная капитализация
+     */
+    @JsonProperty("market_cap")
+    BigDecimal marketCap;
+
+    /**
+     * Изменение цены за последний час в процентах
+     */
+    @JsonProperty("percent_change_1h")
+    BigDecimal percentChange1h;
+
+    /**
+     * Изменение цены за последний день в процентах
+     */
+    @JsonProperty("percent_change_24h")
+    BigDecimal percentChange24h;
+
+    /**
+     * Изменение цены за последние 7 дней в процентах
+     */
+    @JsonProperty("percent_change_7d")
+    BigDecimal percentChange7d;
+
+    /**
+     * Изменение цены за последние 30 дней в процентах
+     */
+    @JsonProperty("percent_change_30d")
+    BigDecimal percentChange30d;
 }
